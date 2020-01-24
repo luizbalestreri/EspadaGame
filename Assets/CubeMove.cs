77448c3d1300b571619fa6826caa82cfe8c31daa
LuizBalestreri <balesss@gmail.com>
2020-01-24 15:53:59 -0300

    Resultado antes do itween

--- Assets/CubeMove.cs
+++ Assets/CubeMove.cs
@@ -4,24 +4,30 @@ using UnityEngine;
 
 public class CubeMove : MonoBehaviour
 {   Vector2 StartPos; //posição para ser usada na movimentação da espada com o dedo
-    float DeltaX; // Delta do x da posição entre um frame e outro utilizado para movimentar o objeto de acordo com a movimentacao do
-    float DeltaY;// Delta do y da posição entre um frame e outro utilizado para movimentar o objeto de acordo com a movimentacao do dedo/mouse
+    public float DeltaX; // Delta do x da posição entre um frame e outro utilizado para movimentar o objeto de acordo com a movimentacao do
+    public float DeltaY;// Delta do y da posição entre um frame e outro utilizado para movimentar o objeto de acordo com a movimentacao do dedo/mouse
     bool mouseDown = false; //utilizado para controlar quando o dedo/mouse está tocando a tela
     Vector3 ScreenToWorld; //Variável para facilitar a escrita do ScreentoWorld na hora de tocar
     Vector3 ScreenToVP;
     Vector3 StartRot;
-    public float velocidade = 1f;
-    
+    public float velocidade = 0.2f;
     Quaternion rotacaoInicial;
+    bool attack = false;
     // Update is called once per frame
+    public float RotX;
+    public float RotY;
+    public float TRotX;
+    public float TRotZ;
+
+    public float i;
     void Start() {
-        rotacaoInicial = transform.rotation;    
+        rotacaoInicial = transform.rotation;
     }
     void Update()
     {//Faz a leitura do input do mouse em World points, fiz essa variavel para facilitar a reescrita
         ScreenToWorld = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));// (Input.mousePosition);
         ScreenToVP = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-        transform.rotation = Quaternion.Lerp(transform.rotation, Camera.main.transform.rotation, velocidade);
+        if (!attack){transform.rotation = Quaternion.Lerp(transform.rotation, Camera.main.transform.rotation, velocidade);}
         if (Input.GetMouseButtonDown(0)){
             mouseDown = true; //ativa a variavel para iniciar a leitura do movimento do dedo)
             StartPos = new Vector3(ScreenToWorld.x,ScreenToWorld.y, 0);
@@ -32,17 +38,34 @@ public class CubeMove : MonoBehaviour
             mouseDown = false;
         }
 
-        if (mouseDown == true){ //se estiver tocando a tela:
+        if (mouseDown && !attack){ //se estiver tocando a tela:
             DeltaX = ScreenToWorld.x - StartPos.x; //variaçao entre a posição atual do dedo no eixo x e a posição do frame anterior (qanto o dedo movimentou)
             DeltaY = ScreenToWorld.y - StartPos.y;// o mesmo no eixo y
-            float RotX = ScreenToVP.x - StartRot.x;
-            float RotY = ScreenToVP.y - StartRot.y;
-            transform.position += new Vector3(DeltaX, DeltaY, 0); //movimenta o objeto de acordo com a movimentacao do dedo
-            transform.Rotate(new Vector3 (-RotY*500, 0, RotX*50)); //faz o objeto rotacionar conforme se move, o objetivo é fazer a parte de baixo (pivot) rotacionar dar um efeito de inclinaçào na hora que move, como se estivesse chacoalhando.            
+            float ClampX = Mathf.Clamp(transform.position.x + DeltaX, -1f, 7.5f); 
+            float ClampY = Mathf.Clamp(transform.position.y + DeltaY, 3f, 14f); 
+            transform.position = new Vector3(ClampX, ClampY, transform.position.z); //movimenta o objeto de acordo com a movimentacao do dedo
             StartPos = new Vector3(ScreenToWorld.x,ScreenToWorld.y, 0); //faz a leitura do toque do frame atual para fazer a comparacao com o proximo
+            
+            RotX = ScreenToVP.x - StartRot.x;
+            RotY = ScreenToVP.y - StartRot.y;
+            TRotX = transform.localRotation.eulerAngles.x - RotY*150;
+            TRotZ = transform.localRotation.eulerAngles.z + RotX*30;
+            float ClampRotX = Mathf.Clamp((TRotX <= 180) ? TRotX : -(360 - TRotX), -40, 90);
+            float ClampRotZ = Mathf.Clamp((TRotZ <= 180) ? TRotZ : -(360 - TRotZ), -10, 10);            
+            transform.localRotation = Quaternion.Euler(new Vector3(ClampRotX, transform.localRotation.eulerAngles.y, ClampRotZ));
             StartRot = new Vector3(ScreenToVP.x, ScreenToVP.y, 0);
-
+            if(TRotX > 45 && RotY < 0){attack = true; Attack();}
         } 
+
+        void Attack(){
+                transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(new Vector3(90, 
+                                transform.localRotation.eulerAngles.y, 
+                                transform.localRotation.eulerAngles.z)),
+                                1);
+            
+            attack = false;
+            Debug.Log("attack!");
+        }
         
     }
 }
